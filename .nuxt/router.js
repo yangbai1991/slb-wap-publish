47fc2ebdb941fb15705aa1545b7e93d6f1468d3a
yangbai <yangbai6644@gmail.com>
2017-11-25 21:09:54 +0800

    add release files

---
+++ .nuxt/router.js
@@ -0,0 +1,68 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const _22448f3e = () => import('../pages/index.vue' /* webpackChunkName: "pages/index" */).then(m => m.default || m)
+const _5426100c = () => import('../pages/user.vue' /* webpackChunkName: "pages/user" */).then(m => m.default || m)
+const _f5a7395c = () => import('../pages/detail.vue' /* webpackChunkName: "pages/detail" */).then(m => m.default || m)
+const _646bece1 = () => import('../pages/cart.vue' /* webpackChunkName: "pages/cart" */).then(m => m.default || m)
+
+
+
+const scrollBehavior = (to, from, savedPosition) => {
+  // SavedPosition is only available for popstate navigations.
+  if (savedPosition) {
+    return savedPosition
+  } else {
+    let position = {}
+    // If no children detected
+    if (to.matched.length < 2) {
+      // Scroll to the top of the page
+      position = { x: 0, y: 0 }
+    }
+    else if (to.matched.some((r) => r.components.default.options.scrollToTop)) {
+      // If one of the children has scrollToTop option set to true
+      position = { x: 0, y: 0 }
+    }
+    // If link has anchor, scroll to anchor by returning the selector
+    if (to.hash) {
+      position = { selector: to.hash }
+    }
+    return position
+  }
+}
+
+
+export function createRouter () {
+  return new Router({
+    mode: 'history',
+    base: '/',
+    linkActiveClass: 'nuxt-link-active',
+    linkExactActiveClass: 'nuxt-link-exact-active',
+    scrollBehavior,
+    routes: [
+		{
+			path: "/",
+			component: _22448f3e,
+			name: "index"
+		},
+		{
+			path: "/user",
+			component: _5426100c,
+			name: "user"
+		},
+		{
+			path: "/detail",
+			component: _f5a7395c,
+			name: "detail"
+		},
+		{
+			path: "/cart",
+			component: _646bece1,
+			name: "cart"
+		}
+    ],
+    fallback: false
+  })
+}
